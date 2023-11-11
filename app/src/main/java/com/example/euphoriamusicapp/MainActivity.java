746fc54fa20392b2ef2bc529d8595aadeb9dd5f8
saitama140203 <lephu9d@gmail.com>
2023-11-12 00:50:00 +0700

    Back-end Euphoria Code 02

--- app/src/main/java/com/example/euphoriamusicapp/MainActivity.java
+++ app/src/main/java/com/example/euphoriamusicapp/MainActivity.java
@@ -1,25 +1,42 @@
 package com.example.euphoriamusicapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     Button btnLogin;
     TextView tvSignUp, tvForgotPassword;
+
+    EditText edtEmail,editPass;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnLogin = findViewById(R.id.btnLogin);
         tvSignUp = findViewById(R.id.tvSignUp);
+        edtEmail = findViewById(R.id.edtEmail);
+        editPass = findViewById(R.id.edtpass);
+        mAuth = FirebaseAuth.getInstance();
         tvForgotPassword = findViewById(R.id.tvForgotPassword);
         tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,10 +55,77 @@ public class MainActivity extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SliderActivity.class);
-                startActivity(intent);
+                btnLogin.setEnabled(false);
+                SignInWithFirebase();
+            }
+        });
+        edtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        editPass.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                checkInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
 
+    }
+
+    private void SignInWithFirebase() {
+        if(!edtEmail.getText().toString().matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@+^[a-zA-Z0-9.-]+$")){
+            mAuth.signInWithEmailAndPassword(edtEmail.getText().toString(),editPass.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Intent intent = new Intent(MainActivity.this,SliderActivity.class);
+                                startActivity(intent);
+                            }else{
+                                Toast.makeText(MainActivity.this, "Login failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                btnLogin.setEnabled(true);
+                            }
+                        }
+                    });
+
+        }else {
+            edtEmail.setError("Invalid Email Pattern!");
+            btnLogin.setEnabled(true);
+        }
+    }
+
+    private void checkInputs() {
+        if(!edtEmail.getText().toString().isEmpty()){
+            if(!editPass.getText().toString().isEmpty()){
+                btnLogin.setEnabled(true);
+            }else {
+                btnLogin.setEnabled(false);
+            }
+        }else {
+            btnLogin.setEnabled(false);
+        }
     }
 }
--- app/src/main/java/com/example/euphoriamusicapp/SignupActivity.java
+++ app/src/main/java/com/example/euphoriamusicapp/SignupActivity.java
@@ -1,20 +1,45 @@
 package com.example.euphoriamusicapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
     ImageButton btnBack;
+    private EditText username;
+    private EditText email;
+    private EditText password;
+    private EditText confirmpassword;
+
+    private Button btn_SinUp;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         btnBack = findViewById(R.id.btnBack);
+        btn_SinUp = findViewById(R.id.btnSignUp);
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        confirmpassword = findViewById(R.id.confirmpassword);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +47,132 @@ public class SignupActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        btn_SinUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SignUpwithFireBase();
+
+
+            }
+        });
+        username.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                CheckInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        email.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                CheckInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                CheckInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        confirmpassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                CheckInputs();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+    private void SignUpwithFireBase() {
+        if(!email.getText().toString().matches("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@+^[a-zA-Z0-9.-]+$")){
+            if(password.getText().toString().equals(confirmpassword.getText().toString())){
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString())
+
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(SignupActivity.this,MainActivity.class);
+                                    startActivity(intent);
+
+                                }else {
+                                    Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    btn_SinUp.setEnabled(true);
+
+                                }
+                            }
+                        });
+            }else {
+                confirmpassword.setError("Password doesn't match.");
+                btn_SinUp.setEnabled(true);
+            }
+
+        }else {
+            email.setError("Invalid Email Pattern!");
+            btn_SinUp.setEnabled(true);
+        }
+    }
+
+    private void CheckInputs() {
+        if(!username.getText().toString().isEmpty()){
+            if(!email.getText().toString().isEmpty()){
+                if(!password.getText().toString().isEmpty()){
+                    if(!confirmpassword.getText().toString().isEmpty()){
+                        btn_SinUp.setEnabled(true);
+                        btn_SinUp.setTextColor(getResources().getColor(R.color.white));
+                    }else {
+                        btn_SinUp.setEnabled(false);
+                        btn_SinUp.setTextColor(getResources().getColor(R.color.transWite));
+                    }
+                }else {
+                    btn_SinUp.setEnabled(false);
+                    btn_SinUp.setTextColor(getResources().getColor(R.color.transWite));
+                }
+            }else {
+                btn_SinUp.setEnabled(false);
+                btn_SinUp.setTextColor(getResources().getColor(R.color.transWite));
+            }
+        }else {
+            btn_SinUp.setEnabled(false);
+            btn_SinUp.setTextColor(getResources().getColor(R.color.transWite));
+        }
     }
 }
